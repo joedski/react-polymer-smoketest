f5d63061efa558b294bddf000358909cef211cb6
Wes Moberly <wesm87@gmail.com>
2016-10-04 17:27:13 -0400

    Rename `.js` files to `.jsx`, update for code standards

---
+++ client/views/App/index.jsx
@@ -0,0 +1,74 @@
+// import ReactPolymer from 'react-polymer';
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import * as Actions from '../../store/actions';
+import PolymerWrapper from '../PolymerWrapper';
+
+class App extends PolymerWrapper {
+  appDropdown(name, onSelectApp) {
+    const itemsJSON = JSON.stringify(
+      this.props.apps.map(a => ({
+        key: a.id,
+        val: a.name,
+      }))
+   );
+
+    const selectedApp = this.props.apps.find(a => a.id === this.props.selectedAppId);
+    const selectedName = selectedApp ? selectedApp.name : '';
+
+    return (
+      <px-dropdown
+        display-value={selectedName}
+        selected-key={selectedApp.id}
+        ref={this.polymerEvents(name, {
+          'selected-key-changed': onSelectApp,
+        })}
+      >
+        <px-dropdown-content
+          items={itemsJSON}
+        />
+      </px-dropdown>
+   );
+  }
+
+  render() {
+    return (
+      <div className="do-something">
+        {this.appDropdown('dropdown', (event) => {
+          console.log(`dropdown => ${event.target.selectedKey}`);
+          this.props.onSelectApp(event.target.selectedKey);
+        })}
+
+        <div className="items">
+          {[0, 1, 2].map(index => (
+            <div className="item" key={index}>
+              {this.appDropdown(`dropdown#${index}`, (event) => {
+                console.log(`dropdown#${index} => ${event.target.selectedKey}`);
+                this.props.onSelectApp(event.target.selectedKey);
+              })}
+            </div>
+         ))}
+        </div>
+      </div>
+   );
+  }
+}
+
+App.propTypes = {
+  apps: PropTypes.arrayOf,
+  selectedAppId: PropTypes.string,
+  onSelectApp: PropTypes.func,
+};
+
+
+const mapState = state => ({
+  apps: state.apps,
+  selectedAppId: state.selection.selectedAppId,
+});
+
+const mapDispatch = dispatch => ({
+  onSelectApp: appId => dispatch(Actions.selectApp(appId)),
+});
+
+
+export default connect(mapState, mapDispatch)(App);
